83cde65ef29b444834933c6eaede80b93625f20c
Linteau <alexandre.linteau.1@ens.etsmtl.ca>
2018-11-26 10:07:12 -0500

    WIP

--- deserializer.vhd
+++ deserializer.vhd
@@ -14,9 +14,44 @@ ENTITY deserializer IS
 END ENTITY;
 
 ARCHITECTURE rtl OF deserializer IS
+  -- SIGNAL --------------------------------------------------------------------
+  SIGNAL working     : STD_LOGIC;
   -- REGISTER ------------------------------------------------------------------
+  SIGNAL started_reg : STD_LOGIC;
+  SIGNAL counter_reg : UNSIGNED(5  DOWNTO 0);
+  SIGNAL data_reg    : STD_LOGIC_VECTOR(31 DOWNTO 0)
 BEGIN
 
-  
+  data_o <= data_reg;
+
+  working <= start_i OR started_reg;
+
+  PROCESS (clk_i)
+  BEGIN
+    IF rising_edge(clk_i) THEN
+      IF clear_i = '1' THEN
+        started_reg <= '0';
+        counter_reg <= (OTHERS=>'0');
+        data_reg    <= (OTHERS=>'0');
+        done_o      <= '0';
+      ELSE
+        started_reg <= started_reg;
+        counter_reg <= counter_reg;
+        data_reg    <= data_reg;
+        done_o      <= done_o;
+        IF start_i = '1' THEN
+          started_reg <= '1';
+        END IF;
+        IF working = '1' THEN
+          counter_reg <= counter_reg + 1;
+          data_reg <= data_reg(30 DOWNTO 0) & data_i;
+          IF counter_reg = ((OTHERS=>'1')) THEN
+            started_reg <= '0';
+            done_o      <= '1';
+          END IF;
+        END IF;
+      END IF;
+    END IF;
+  END PROCESS;
 
 END ARCHITECTURE;
--- serializer.vhd
+++ serializer.vhd
@@ -14,9 +14,39 @@ ENTITY serializer IS
 END ENTITY;
 
 ARCHITECTURE rtl OF serializer IS
+  -- SIGNAL --------------------------------------------------------------------
+  SIGNAL working     : STD_LOGIC;
   -- REGISTER ------------------------------------------------------------------
+  SIGNAL started_reg : STD_LOGIC;
+  SIGNAL counter_reg : UNSIGNED(6  DOWNTO 0);
 BEGIN
 
-  
+  working <= start_i OR started_reg;
+
+  PROCESS (clk_i)
+  BEGIN
+    IF rising_edge(clk_i) THEN
+      IF clear_i = '1' THEN
+        started_reg <= '0';
+        counter_reg <= (OTHERS=>'0');
+        done_o      <= '0';
+      ELSE
+        started_reg <= started_reg;
+        counter_reg <= counter_reg;
+        done_o      <= done_o;
+        IF start_i = '1' THEN
+          started_reg <= '1';
+        END IF;
+        IF working = '1' THEN
+          counter_reg <= counter_reg + 1;
+          data_o      <= data_i(to_integer(counter_reg));
+          IF counter_reg = to_unsigned(47, counter_reg'length) THEN
+            started_reg <= '0';
+            done_o      <= '1';
+          END IF;
+        END IF;
+      END IF;
+    END IF;
+  END PROCESS;
 
 END ARCHITECTURE;
